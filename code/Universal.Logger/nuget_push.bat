a96569657129ad559ec54d2878c1a1d9ba1f052c
Accentient <support@accentient.com>
2017-04-20 10:12:31 -0600

    Changed the path to nuget.exe

--- code/Universal.Logger/nuget_push.bat
+++ code/Universal.Logger/nuget_push.bat
@@ -1,3 +1,3 @@
-"c:\program files\microsoft team foundation server 15.0\tools\nuget.exe" push -Source "Universal" -ApiKey myKey Universal.Logger.1.0.0.nupkg
+"c:\course\software\nuget.exe" push -Source "Universal" -ApiKey myKey Universal.Logger.1.0.0.nupkg
 
 pause
